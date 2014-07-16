0cd0f3ed33ff0a3e2816871c00508078790b7391
Simon DotOrg <simon.schmidt@htp-tel.de>
2014-07-16 14:55:13 +0200

    Update nw.c

--- nwlib/nw.c
+++ nwlib/nw.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014, Simon Schmidt
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 
